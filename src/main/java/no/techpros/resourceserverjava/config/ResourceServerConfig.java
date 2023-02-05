3dca08b9a0f35cdb4efef9ba67d4a74d6bdfb822
tore gard <dragerot@gmail.com>
2023-02-05 14:18:13 +0100

    Rydder litt

--- src/main/java/no/techpros/resourceserverjava/config/ResourceServerConfig.java
+++ src/main/java/no/techpros/resourceserverjava/config/ResourceServerConfig.java
@@ -1,12 +1,11 @@
 package no.techpros.resourceserverjava.config;
 
-import com.c4_soft.springaddons.security.oauth2.config.ConfigurableClaimSet2AuthoritiesConverter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 @Configuration
 @EnableWebSecurity
-@EnableMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
+@EnableMethodSecurity
 public class ResourceServerConfig {
 
 }
